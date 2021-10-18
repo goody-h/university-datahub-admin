e23a7777885d59b2866f07c5295bcad118c0513e
Goodhope Ordu <goodhopeordu@yahoo.com>
2021-10-19 00:14:58 +0100

    Update Modify row for master sheet

--- transformers/master_sheet.py
+++ transformers/master_sheet.py
@@ -24,7 +24,8 @@ class MasterSheet(TableMapper):
             'resultId': self.getResultId(row['mat_no'].upper())
         })
         # TODO tests and sanitize (mat number, score), yada yada yada!
-        return row.get('score') != None and re.fullmatch('^\d+\.{0,1}\d*$',str(row.get('score'))) != None
+        return ((row.get('score') != None and re.fullmatch('^\d+\.{0,1}\d*$',str(row.get('score'))) != None) 
+            or str(row.get('delete')).lower() == 'true')
     
     def __is_valid_header__(self):
         return self.session != '' and self.courseCode != ''
