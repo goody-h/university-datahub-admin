f349a1f2f24f3f11ff4b5faca59e814bd9d93a6a
Goodhope Ordu <goodhopeordu@yahoo.com>
2021-11-03 19:49:10 +0100

    upload mastersheet with header-data in rows

--- transformers/master_sheet.py
+++ transformers/master_sheet.py
@@ -2,7 +2,7 @@ import re
 from transformers.table_mapper import TableMapper
 
 class MasterSheet(TableMapper):
-    def __init__(self, filename, courseCode='', session=''):
+    def __init__(self, filename, courseCode=None, session=None):
         super().__init__(
             filename, table_anchor='matric',
             row_check = {'match': '^[u,U]\d{4}/\d{7}$', 'key': 'mat_no'},
@@ -10,6 +10,8 @@ class MasterSheet(TableMapper):
                 {'search': 'matric', 'key': 'mat_no'},
                 {'search': '(total|score)', 'key': 'score'},
                 {'search': 'delete', 'key': 'delete'},
+                {'search': 'session', 'key': 'session'},
+                {'search': 'course (code|no)', 'key': 'code'},
             ],
             header_map = [{'search': 'session', 'exec': self.session_handler}, {'search': 'course (code|no)', 'exec': self.code_handler}]),
             
@@ -18,17 +20,32 @@ class MasterSheet(TableMapper):
         self._courseCode = courseCode
 
     def __modify_row__(self, row):
+        if re.match('^(_){0,1}(\d){4}/(\d){4}(_){0,1}$', str(row.get('session'))) != None:
+            row['session'] = int(row['session'].strip().replace('_','').split('/')[1])
+        elif self.session != None:
+            row['session'] = self.session
+        else:
+            return False
+        if re.match('^(_){0,1}[A-z]{3}(\s|_){0,1}\d{3}\.\d(_){0,1}', str(row.get('code'))) != None:
+            c1 = row['code'].replace('_', '').replace(' ', '').lower()
+            c2 = re.split('([a-z]{3})(\d{3})\.(\d)', c1)
+            courseCode = c2[1] + '_' + c2[2] + '_' + c2[3]
+            row.update({'courseCode': courseCode, 'courseId': courseCode})
+        elif self.courseCode != None:
+            row.update({'courseCode': self.courseCode, 'courseId': self.courseCode})
+        else:
+            return False
+
         row.update({
-            'session': self.session, 'courseId': self.courseId,
-            'courseCode': self.courseCode, 'mat_no': row['mat_no'].upper(),
-            'resultId': self.getResultId(row['mat_no'].upper())
+            'mat_no': row['mat_no'].upper(),
+            'resultId': self.getResultId(row['mat_no'].upper(), row['session'], row['courseCode'])
         })
         # TODO tests and sanitize (mat number, score), yada yada yada!
         return ((row.get('score') != None and re.fullmatch('^\d+\.{0,1}\d*$',str(row.get('score'))) != None) 
             or str(row.get('delete')).lower() == 'true')
     
-    def __is_valid_header__(self):
-        return self.session != '' and self.courseCode != ''
+    # def __is_valid_header__(self):
+        # return self.session != '' and self.courseCode != ''
 
     def __post_header_call__(self):
         if self.session == None:
@@ -60,23 +77,9 @@ class MasterSheet(TableMapper):
         self.courseCode = c2[1] + '_' + c2[2] + '_' + c2[3]
         self.courseId = self.courseCode
 
-    def getResultId(self, mat_no):
-        return str(self.session) + '-' + self.courseId + '-' + mat_no.replace('/', '-')
+    def getResultId(self, mat_no, session, code):
+        return str(session) + '-' + code + '-' + mat_no.replace('/', '-')
    
     def get_results(self):
         return self.get_data()
 
-if __name__ == '__main__':
-    # Run a test using sample master sheet
-    root = ''
-    if __file__ != None:
-        root = re.sub('/[^/]+$', '/', __file__.replace('\\', '/')) + '../'
-
-    master = MasterSheet(root + 'static/excel/ENG301.1.xlsx', courseCode='chm_130_1', session=2019)
-    results = master.get_results()
-    batch = master.batchId
-    code = master.courseCode
-    session = master.session
-
-    for data in results:
-        print(data)
